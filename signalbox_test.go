e72e1c5460da703e17920b2dd1a5d5927b508b1f
clinton <clinton.freeman@nicta.com.au>
2014-04-23 20:28:08 +1000

    Basic websocket connection test.

---
+++ signalbox_test.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Clinton Freeman 2014
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"testing"
+)
+
+func TestSocket(t *testing.T) {
+	go main()
+
+	// Open socket.
+	origin := "http://localhost/"
+	url := "ws://localhost:3000/echo"
+	ws, err := websocket.Dial(url, "", origin)
+
+	if err != nil {
+		t.Errorf("Unable open websocket to signalbox.")
+		return
+	}
+
+	// Close socket.
+	err = ws.Close()
+	if err != nil {
+		t.Errorf("Unable to close websocket to signalbox.")
+		return
+	}
+}
